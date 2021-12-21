b247db469c47dee6d3e62d6a788a841cb473cbcc
Shelsy Elaine <elainesbarrera@gmail.com>
2021-12-21 13:29:38 -0800

    ADDED: future menu layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import ServicesMenu from './components/ServicesMenu';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <ServicesMenu/>
     </div>
   );
 }
---
+++ src/components/ServicesMenu.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ServicesMenu = () => {
+  return (
+    <div id="container" class="container-sm">
+      <div class="row">
+        <div class="col">
+          <div class="card home-menu">
+            <div class="card-body">Option 1</div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card home-menu">
+            <div class="card-body">Option 2</div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card home-menu">
+            <div class="card-body">Option 3</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesMenu;
