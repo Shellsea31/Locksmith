643fffbb1a36571f454b72525aab8ae5be433f25
Shelsy Elaine <elainesbarrera@gmail.com>
2021-12-20 18:23:01 -0800

    ADDED: navbar styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav class="border-0 navbar">
+      <div class="container-fluid">
+        <a id="navbarTitle" class="navbar-brand" href="#">
+          <i class="fas fa-key me-2"></i>
+          R&D Locksmith
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
